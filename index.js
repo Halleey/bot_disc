c277b47cd22ed46cf2aea0e9e49ae77fd16e90d5
hallyson <aparecidohallyson@gmail.com>
2024-11-11 15:05:15 -0300

    add profile player BS

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const commandHandler = require('./slashCommands/handleCommand');
 const buttonInteractions = require('./automato/buttonInteractions');
 const messageHandler = require('./automato/messageHandler');
 const guildMemberHandler = require('./automato/guildMemberHandler');
+const antLink = require('./automato/antLink');
+const { showPlayerProfile } = require('./automato/perfil'); // Importa a função de perfil do jogador
+
+// Criação do cliente
 const client = clientConfig.createClient();
-const  antLink = require('./automato/antLink')
+
 // Quando o bot estiver pronto
 client.on('ready', clientConfig.onReady);
 
@@ -19,12 +23,21 @@ client.on('interactionCreate', buttonInteractions.handleButton);
 // Mensagens contendo "!bot"
 client.on('messageCreate', messageHandler.handleMessage);
 
-client.on('messageCreate', (message) => 
-{
-    if(message.author.bot) return;
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return;
 
-    antLink.blockLinks(message)
-})
+    antLink.blockLinks(message);
+
+    // Comando para exibir perfil do jogador Brawl Stars
+    if (message.content.startsWith('!player')) {
+        const playerTag = message.content.split(' ')[1];
+        if (playerTag) {
+            showPlayerProfile(playerTag, message); // Chama a função para exibir o perfil
+        } else {
+            message.reply('Por favor, forneça a tag do jogador. Exemplo: `!player B8J9V9L2Q`');
+        }
+    }
+});
 
 // Atualizações no servidor
 client.on('guildMemberUpdate', guildMemberHandler.handleGuildMemberUpdate);
